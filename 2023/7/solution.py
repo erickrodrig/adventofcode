dffd6ea86c3ded0133cd3a15e5e26f400a2d6c3e
Erick Rodrigues <erick.rod1100@gmail.com>
2024-03-12 23:59:37 -0300

    Day 7

---
+++ 2023/7/solution.py
@@ -0,0 +1,16 @@
+cards = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
+
+ranks = {}
+
+hands = {
+    "32T3K": 765,
+    "T55J5": 684,
+    "KK677": 28,
+    "KTJJT": 220,
+    "QQQJA": 483,
+}
+
+print(len(hands))
+
+for hand, bid in hands.items():
+    pass
